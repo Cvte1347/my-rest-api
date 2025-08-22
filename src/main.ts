af313f13d5175e17f5788b54f8d0b49669464c67
qxoo <overlap123123@gmail.com>
2025-08-22 20:03:46 +0300

    фывячс

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   await app.listen(process.env.PORT ?? 3000);
-}
+  Logger.log(`[Backend] started at port: ${process.env.PORT ?? 3000}`);
+};
+
 bootstrap();
